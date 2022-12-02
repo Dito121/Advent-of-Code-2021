a57cc8916500b8b457bba6db308a8317fd176af6
Gerardo Ayes <gerardoayes92@gmail.com>
2022-12-01 22:22:50 -0500

    solved puzzle 2 part 1

--- day_2/puzzle_2.py
+++ day_2/puzzle_2.py
@@ -1,4 +1,27 @@
 class Solution:
+    def __init__(self, file: str, horizon: int = 0, depth: int = 0):
+        self.horizon = horizon
+        self.depth = depth
+        self.file = file
 
-    def __init__(self, horiz: int = 0, depth: int = 0):
-        
+        with open(self.file) as file:
+            for line in file:
+                if "forward" in line:
+                    self.horizon += int(line.strip()[-1])
+                elif "down" in line:
+                    self.depth += int(line.strip()[-1])
+                elif "up" in line:
+                    self.depth -= int(line.strip()[-1])
+                else:
+                    continue
+
+    def solve_part_1(self):
+        return self.horizon * self.depth
+
+    def solve_part_2(self):
+        pass
+
+
+# answer = Solution("day_2/puzzle_2_data.txt")
+# print("Puzzle 2 Part 1 Solution: ", answer.solve_part_1())
+# print("Puzzle 2 Part 2 Solution: ", answer.solve_part_2())
---
+++ day_2/tests_puzzle_2.py
@@ -0,0 +1,14 @@
+import unittest
+from puzzle_2 import Solution
+
+
+class test_problems(unittest.TestCase):
+    def test_puzzle_2(self):
+        puzzle2 = Solution("day_2/puzzle_2_sample_data.txt")
+
+        self.assertEqual(puzzle2.solve_part_1(), 150)
+        # self.assertEqual(puzzle2.solve_part_2(), )
+
+
+if __name__ == "__main__":
+    unittest.main()
